7737e7fde4f9a09de822bee779c1d0e16ff5cdbd
Anders Einar Hilden <hildenae@gmail.com>
2013-10-09 22:46:25 +0200

    Remove all debian/*.ex

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libfyba):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libfyba" command="/usr/bin/libfyba"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibfyba 4.1.0 libfyba (>> 4.1.0-0), libfyba (<< 4.1.0-99)
