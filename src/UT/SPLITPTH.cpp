cbdb198d97480ca3554feeed4ea626994ad52b7c
Anders Einar Hilden <hildenae@gmail.com>
2013-10-21 12:56:12 +0200

    Make SPLITPTH.cpp work (better) on Windows

--- src/UT/SPLITPTH.cpp
+++ src/UT/SPLITPTH.cpp
@@ -4,11 +4,22 @@
 //        UT_splitpath - split a full path name
 //
 
+#include "stdafx.h"
 #include <cstring>
-#include <fyut.h>
 
-void UT_splitFN(char *filename, char* name, char* ext) {
-	char* lastDot = strrchr(filename, '.');
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#include "fyut.h"
+
+#ifdef LINUX
+#define wchar_t char
+#define wcslen  strlen
+#endif
+
+void UT_splitFN(wchar_t *filename, wchar_t* name, wchar_t* ext) {
+	wchar_t* lastDot = strrchr(filename, '.');
 	if(lastDot != NULL) {
 		UT_StrCopy(ext, lastDot, _MAX_EXT);
 		if(strlen(filename) > _MAX_EXT) {
@@ -33,19 +44,19 @@ CD Splits a path into drive (X: for windows, "" for linux),
 CD directory, filename (w/o extension) and extension.
 CD
 CD Parameters:
-CD Type        Name             I/O  Explanation
+CD Type           Name             I/O  Explanation
 CD -------------------------------------------------------------
-CD const char *pathP             i  Path to split
-CD char       *driveP            o  Drive-part of path
-CD char       *dirP              o  Directory-part
-CD char       *nameP             o  Filename w/o extension
-CD char       *extP              o  extension
+CD const wchar_t *pathP             i   Path to split
+CD wchar_t       *driveP            o   Drive-part of path
+CD wchar_t       *dirP              o   Directory-part
+CD wchar_t       *nameP             o   Filename w/o extension
+CD wchar_t       *extP              o   Extension
 CD  ==============================================================
 */
-SK_EntPnt_UT void UT_splitpath(const char *pathP, char *driveP, char *dirP, char *nameP, char *extP) {
-	char local_path[PATH_MAX]; /* Copy of pathP i case we modify it */
-	char tmp[PATH_MAX]; /* Copy of pathP i case we modify it */
-	char filename[PATH_MAX];
+SK_EntPnt_UT void UT_splitpath(const wchar_t *pathP, wchar_t *driveP, wchar_t *dirP, wchar_t *nameP, wchar_t *extP) {
+	wchar_t local_path[_MAX_PATH]; /* Copy of pathP i case we modify it */
+	wchar_t tmp[_MAX_PATH]; /* Copy of pathP i case we modify it */
+	wchar_t filename[_MAX_PATH];
 	(*driveP) = (*dirP) = (*nameP) = (*extP) = '\0';
 
 	strcpy(local_path, pathP);
@@ -53,11 +64,11 @@ SK_EntPnt_UT void UT_splitpath(const char *pathP, char *driveP, char *dirP, char
 	/* Under linux, driveP is always \0 */
 	#ifdef WIN32
 		/* Afaik, there is only ONE : in windows filenames */
-		char* theColon = strrchr(tmp, ':');
+		wchar_t* theColon = strrchr(tmp, ':');
 		if(theColon != NULL) {
 			/* We overwrite local_path here, because after this the code
 			is equal for win/lin if the drive-part is removed */
-			UT_StrCopy(local_path, theColon+1, PATH_MAX);
+			UT_StrCopy(local_path, theColon+1, _MAX_PATH);
 			(*(theColon + 1)) = '\0'; // set a \0 after the color (inside tmp!)
 			UT_StrCopy(driveP, tmp, _MAX_DRIVE);
 			if (strlen(tmp) > _MAX_DRIVE) { // how would this even happen?
@@ -68,12 +79,12 @@ SK_EntPnt_UT void UT_splitpath(const char *pathP, char *driveP, char *dirP, char
 
 	strcpy(tmp, local_path);
 	
-	char* lastSlash = strrchr(tmp, UT_SLASH);
+	wchar_t* lastSlash = strrchr(tmp, UT_SLASH);
 	
 	/* Set dirP */
 	if(lastSlash != NULL) {
 		/* +1 because we don't want the / in the filename */
-		char filename[PATH_MAX]; /* UT_splitFN might modify filename */
+		wchar_t filename[_MAX_PATH]; /* UT_splitFN might modify filename */
 		strcpy(filename,lastSlash+1);
 		if (strcmp(filename, ".") != 0) {
 			UT_splitFN(filename, nameP, extP);
